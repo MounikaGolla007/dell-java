aac179e1022da54604a39a05abf8778650ce9bec
Golla <Mounika_Golla@Dell.com>
2019-01-09 19:47:49 -0600

    RockPaperScissors

---
+++ RockPaperScissors/src/RockPaperScissors.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+import java.util.EnumSet;
+import java.util.Random;
+public class RockPaperScissors {
+	public static void main(String[] args) {		
+		String userHand;
+		String computerHand;		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Choose a Hand from below");
+		EnumSet.allOf(Options.class).forEach(option ->System.out.println(option));		
+		userHand = sc.nextLine();
+		computerHand = GenerateRandomHand().toString();
+		CompareHand(userHand,computerHand);
+		sc.close();
+	}
+	
+	public static Options GenerateRandomHand()
+	{
+		Options option = Options.values()[(int)(Math.random()*Options.values().length)]; 
+		return option;		
+	}
+	
+	public static void CompareHand(String userHand , String randomHand)
+	{
+		if(userHand.equals(randomHand))
+		{
+			System.out.println("User and Computer hand are same.Game Draw!!");			
+		}
+		else if(userHand.equals(Options.Rock.toString()) &&  randomHand.equals(Options.Scissors.toString()))
+		{
+			System.out.println("Computer Won!!");	
+		}
+		else if(userHand.equals(Options.Rock.toString()) &&  randomHand.equals(Options.Paper.toString()))
+		{
+			System.out.println("Computer Won!!");
+		}
+		else if(userHand.equals(Options.Scissors.toString()) &&  randomHand.equals(Options.Paper.toString()))
+		{
+			System.out.println("User Won!!");
+		}	
+		else if(userHand.equals(Options.Scissors.toString()) &&  randomHand.equals(Options.Rock.toString()))
+		{
+			System.out.println("Computer Won!!");	
+		}
+		else if(userHand.equals(Options.Paper.toString()) &&  randomHand.equals(Options.Rock.toString()))
+		{
+			System.out.println("User Won!!");
+		}
+		else if(userHand.equals(Options.Paper.toString()) &&  randomHand.equals(Options.Scissors.toString()))
+		{
+			System.out.println("Computer Won!!");
+		}		
+	}
+	
+	public enum Options{
+		Rock,
+		Paper,
+		Scissors
+	}
+
+}
